b8c34c330e11efdcf2d7c54e6dc886829476bd13
Theo Wadsley <theo.wadsley@gmail.com>
2015-09-26 17:25:23 +1000

    Added checks for robustness in case powerline-status is missing

--- vim/settings/yadr-prompt.vim
+++ vim/settings/yadr-prompt.vim
@@ -1,2 +1,4 @@
 " Powerline prompt
-source "$POWERLINE_REPOSITORY_ROOT/powerline/bindings/vim/plugin/powerline.vim"
+if filereadable(expand("$POWERLINE_REPOSITORY_ROOT/powerline/bindings/vim/plugin/powerline.vim"))
+  source "$POWERLINE_REPOSITORY_ROOT/powerline/bindings/vim/plugin/powerline.vim"
+endif
--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,3 +1,4 @@
-# prompt skwp
-export POWERLINE_REPOSITORY_ROOT=`pip show powerline-status | grep '^Location:' | awk '{ print $2 }'`
-source "$POWERLINE_REPOSITORY_ROOT/powerline/bindings/zsh/powerline.zsh"
+which pip >/dev/null && export POWERLINE_REPOSITORY_ROOT=`pip show powerline-status | grep '^Location:' | awk '{ print $2 }'`
+if [ -f "$POWERLINE_REPOSITORY_ROOT/powerline/bindings/zsh/powerline.zsh" ]; then
+  source "$POWERLINE_REPOSITORY_ROOT/powerline/bindings/zsh/powerline.zsh"
+fi
